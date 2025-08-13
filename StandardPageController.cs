89e09dc5298af3a8018f4a1582de303f22693c67
kongehund <63306696+kongehund@users.noreply.github.com>
2025-08-13 14:36:34 +0200

    WIP add Standard Page
    The metadata should be editable, but the front-end does not seem to work yet.

---
+++ StandardPageController.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Core;
+using EPiServer.Web.Mvc;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Mvc;
+using OptimizelyCMSTest.Models.Pages;
+using System;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Shared.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        [HttpGet]
+        public IActionResult Index(StandardPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
