ff4d066998c444e6c29ac79246a0f83f7ccdd3e5
kongehund <63306696+kongehund@users.noreply.github.com>
2025-08-14 08:30:04 +0200

    Add website in CMS admin settings

---
+++ Controllers/StandardPageController.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Core;
+using EPiServer.Web.Mvc;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Mvc;
+using OptimizelyCMSTest.Models.Pages;
+using System;
+
+namespace OptimizelyCMSTest.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        [HttpGet]
+        public IActionResult Index(StandardPage currentPage)
+        {
+            return View(currentPage);
+        }
+        //public IActionResult Index(StandardPage currentPage)
+        //{
+        //    return Content($"Page found! Title: {currentPage.Title}");
+        //}
+    }
+}
---
+++ Models/Pages/AnotherPage.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace OptimizelyCMSTest.Models.Pages;
+
+[ContentType(
+    DisplayName = "Another page",
+    Description = "Another page, yep...",
+    GUID = "f1049bf4-ad59-4fb0-97c0-df4f4e110092",
+    GroupName = "My Test Pages")]
+public class AnotherPage : PageData
+{
+    [Display(
+        Name = "HeadLine",
+        GroupName = SystemTabNames.Content,
+        Order = 10)]
+    public virtual string HeadLine { get; set; }
+
+    [Display(
+        Name = "HeadLine Slug",
+        GroupName = SystemTabNames.Content,
+        Order = 10)]
+    public virtual string HeadLineSlug { get; set; }
+
+    [Display(
+        Name = "Body",
+        GroupName = SystemTabNames.Content,
+        Order = 10)]
+    public virtual XhtmlString Body { get; set; }
+}
